c042d9d486845a269ce2056cb389ee94585b00e6
shahriarXmode <137916535+shahriarXmode@users.noreply.github.com>
2023-06-27 22:06:23 +0600

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,15 @@
+def generate_banner():
+    banner = r"""
+ _____       _     _     _ _
+|  __ \     (_)   | |   | (_)
+| |__) |__ _ _  __| | __| |_ _ __   __ _
+|  ___/ __| |/ _` |/ _` | | '_ \ / _` |
+| |   \__ \ | (_| | (_| | | | | | (_| |
+|_|   |___/_|\__,_|\__,_|_|_| |_|\__, |
+                                __/ |
+                               |___/
+"""
+    print(banner)
+
+# Call the function to generate the banner
+generate_banner()
