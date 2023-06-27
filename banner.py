8580fe040b8434374c734e1d7cb72098699183e9
shahriarXmode <137916535+shahriarXmode@users.noreply.github.com>
2023-06-27 23:45:45 +0600

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,20 +1,20 @@
 def generate_banner():
     banner = r"""
-  ____  _   _    _    _   _ ____  ___    _    ____   
+\033[91m  ____  _   _    _    _   _ ____  ___    _    ____   
  / ___|| | | |  / \  | | | |  _ \|_ _|  / \  |  _ \  
  \___ \| |_| | / _ \ | |_| | |_) || |  / _ \ | |_) | 
   ___) |  _  |/ ___ \|  _  |  _ < | | / ___ \|  _ <  
  |____/|_| |_/_/   \_\_| |_|_| \_\___/_/   \_\_| \_\ 
                                                      
-"""
+\033[0m"""
     print(banner)
 
 def menu():
     print("Welcome to Your Termux Tool!")
-    print("1. Option 1")
-    print("2. Option 2")
-    print("3. Option 3")
-    print("4. Exit")
+    print("\033[92m1. Option 1\033[0m")
+    print("\033[92m2. Option 2\033[0m")
+    print("\033[92m3. Option 3\033[0m")
+    print("\033[92m4. Exit\033[0m")
 
     choice = input("Enter your choice (1-4): ")
 
